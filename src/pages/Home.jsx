f07fc44f398e0ee5f3225aae2087279797e7cd1c
UJJWAL KUMAR <uky3045@gmail.com>
2024-11-21 10:06:03 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+const Home = () => {
+  // storing the exercises searched through input field in a state variable
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} bodyPart={bodyPart} exercises={exercises} />
+    </Box>
+  );
+};
+
+export default Home;
